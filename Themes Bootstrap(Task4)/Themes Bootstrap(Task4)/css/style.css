d3b42ef15927c9cb7e9a525d92c7b2bf7215e506
Izzataa <129064478+Izzataa@users.noreply.github.com>
2023-05-15 04:07:11 +0400

    upd

---
+++ Themes Bootstrap(Task4)/Themes Bootstrap(Task4)/css/style.css
@@ -0,0 +1,270 @@
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+a{
+    text-decoration: none;
+}
+.A{
+background-image: url(https://startbootstrap.github.io/startbootstrap-creative/assets/img/bg-masthead.jpg);
+   height: 100vh;
+   background-position: center;
+   background-size: cover;
+   text-align: center;
+}
+.A1{
+    height: 100vh;
+    background-color: rgba(92, 77, 66, 0.8);
+    opacity: 0.7;
+}
+.A2 a{
+    color: #fff;
+float: left;
+font-size: 20px;
+margin-top: 20px;
+padding: 0px 0px 0px 300px ;
+}
+.A2 a{
+    color: #fff;
+    float: right;
+    margin: 20px 29px 0px -269px;
+    padding: 0px 274px 0px 0px;
+}
+.A3 h1{
+    color: #fff;
+    width: 44%;
+    text-align: center;
+    font-size: 4rem;
+    font-weight: 100;
+    margin-left: 29%;
+    padding: 15% 0% 2%;
+}
+.A3 hr{
+    width: 60px;
+    text-align: center;
+    margin-left: 49%;
+    height: 1.7px;
+    background-color: #f4623a;
+    border: #f4623a;
+    opacity: 0.7;
+}
+.A4{
+    color: #fff;
+    text-align: center;
+}
+.A4 p{
+    font-size: 1.4rem;
+    opacity: 0.6;
+    width: 45%;
+    font-weight: 900;
+    padding-bottom: 29px;
+    margin: 10px 0px 49px;
+    margin-left: 29%;
+    line-height: 1.5;
+}
+.A4 a{
+    color: #F4623A;
+font-size: 15px;
+margin-left: 1%;
+padding: 20px 36px;
+border-radius: 50px;
+background-color: #db3507;
+}
+.B{
+    height: 40vh;
+    background-position: center;
+   background-size: cover;
+   text-align: center;
+   background-color: #fa582c;
+   opacity: 0.8;
+}
+.B h1{
+    font-size: 3rem;
+    padding-top: 100px;
+    margin-bottom: 20px;
+color: #fff;
+}
+.B hr{
+    width: 60px;
+    text-align: center;
+    margin-left: 49%;
+    height: 3px;
+    background-color: #fff;
+    border: #fff;
+    opacity: 0.9;
+}
+.B p{
+    color: #fff;
+    font-size: 1.2rem;
+    opacity: 0.8;
+    width: 45%;
+    font-weight: 100;
+    padding-bottom: 30px;
+    margin: 10px 0px 20px;
+    margin-left: 29%;
+    line-height: 1.5;
+}
+.B a{
+    color: #000;
+    font-size: 15px;
+    font-weight: 900;
+    margin-left: 1%;
+    padding: 20px 36px;
+    border-radius: 50px;
+    background-color: #fff;
+}
+.C{
+    background-color: #fff;
+    height: 40vh;
+    background-position: center;
+    background-size: cover;
+    text-align: center;
+}
+.C h1{
+    font-size: 2rem;
+    padding-top: 100px;
+    margin-bottom: 20px;
+}
+.C hr{
+    width: 60px;
+    text-align: center;
+    margin-left: 49%;
+    height: 3px;
+    background-color: #fa582c;
+    border: #fa582c;
+    opacity: 0.9;
+}
+.D{
+    width: 100%;
+    
+    
+}
+.D1{
+    width: 23%;
+    float: left;
+
+}
+.D2{
+    width: 23%;
+    float: left;
+    
+}
+.D3{
+    width: 23%;
+    float: left;
+}
+.D4{
+    width: 23%;
+    float: left;
+}
+.D h1{
+    padding: 30px 0px 10px;
+}
+.D i{
+    font-size: 30px;
+    margin: 50px 0px 0px;
+}
+.D p{
+    width: 65%;
+    margin-left: 16%;
+    font-size: 20px;
+    margin-bottom: 150px;
+}
+.E{
+    height: 90vh;
+    width: 100%;
+}
+.E{
+    float: left;
+    width: calc(100%/3);
+margin: -2px
+}
+.F{
+    background-color: #212529;
+    height: 40vh;
+    background-position: center;
+    background-size: cover;
+    text-align: center;
+}
+.F h1{
+    color: #fff;
+padding-top: 170px;
+margin-bottom: 40px;
+}
+.F a{
+    color: #000;
+    font-size: 15px;
+    font-weight: 900;
+    padding: 20px 20px;
+    border-radius: 50px;
+    background-color: #fff;
+}
+.G{
+    height: 70vh;
+    background-position: center;
+  background-size: cover;
+  text-align: center;
+  width: 100%;
+}
+.G h1{
+    margin: 150px 0px 20px 0px;
+
+    
+}
+.G hr{
+    width: 60px;
+    text-align: center;
+    margin-left:  49%;
+    height: 3px;
+    background-color: #fa582c;
+    border: #fa582c;
+    opacity: 0.9;
+
+}
+.G p{
+    margin: 30px 0px 0px 670px;
+    width: 30%;
+   color: #6c757d;
+   font-size: 16px;
+   font-weight: 300;
+}
+.G1{
+    margin: 30px 0px 5px 670px;
+     width: 30%;
+    
+}
+.G2{
+    width: 100%;
+    margin: 10px 0px;
+    padding: 20px 5px;
+    font-weight: 700;
+    border-radius: 10px;
+    border: 1px solid #6c757d;
+    outline-color: #db3507;
+    
+}
+.G3{
+    padding: 10px 0px 80px 5px;
+    margin: 10px 0px 40px ;
+    
+}
+.G a{
+    width: 60%;
+    color: #fff;
+    font-size: 15px;
+    font-weight: 900;
+    padding: 20px 250px;
+    border-radius: 50px;
+    background-color: #fa582c;
+    opacity: 0.5;
+}
+.G i {
+    font-size: 30px;
+
+}
+.G .p1
+{
+    font-size: 20px;
+    
+}
